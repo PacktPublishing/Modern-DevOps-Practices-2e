a48d938be2e532489ee164a0956fed84d8b062ac
Gaurav Agarwal <bharatmicrosystems@gmail.com>
2023-09-16 08:07:43 +0000

    Updated ch13

--- ch13/binaryauth/binaryauth.tf
+++ ch13/binaryauth/binaryauth.tf
@@ -1,4 +1,5 @@
 resource "google_kms_key_ring" "qa-attestor-keyring" {
+  count = var.branch == "dev" ? 1 : 0
   name     = "qa-attestor-keyring"
   location = var.region
   lifecycle {
@@ -7,19 +8,21 @@ resource "google_kms_key_ring" "qa-attestor-keyring" {
 }
 
 module "qa-attestor" {
+  count = var.branch == "dev" ? 1 : 0
   source = "terraform-google-modules/kubernetes-engine/google//modules/binary-authorization"
   attestor-name = "quality-assurance"
   project_id    = var.project_id
-  keyring-id    = google_kms_key_ring.qa-attestor-keyring.id
+  keyring-id    = google_kms_key_ring.qa-attestor-keyring[0].id
 }
 
 resource "google_binary_authorization_policy" "policy" {
+  count = var.branch == "dev" ? 1 : 0
   global_policy_evaluation_mode = "ENABLE"
   default_admission_rule {
     evaluation_mode  = "REQUIRE_ATTESTATION"
     enforcement_mode = "ENFORCED_BLOCK_AND_AUDIT_LOG"
     require_attestations_by = [
-      module.qa-attestor.attestor
+      module.qa-attestor[0].attestor
     ]
   }
 }
