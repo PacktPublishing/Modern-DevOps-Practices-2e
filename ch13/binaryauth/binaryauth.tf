cc5e5208239a8c353695ce076143cda3582c58b4
Gaurav Agarwal <bharatmicrosystems@gmail.com>
2023-09-16 15:45:28 +0000

    Updated

--- ch13/binaryauth/binaryauth.tf
+++ ch13/binaryauth/binaryauth.tf
@@ -17,6 +17,33 @@ module "qa-attestor" {
 
 resource "google_binary_authorization_policy" "policy" {
   count = var.branch == "dev" ? 1 : 0
+  admission_whitelist_patterns {
+    name_pattern = "gcr.io/google_containers/*"
+  }
+  admission_whitelist_patterns {
+    name_pattern = "gcr.io/google-containers/*"
+  }
+  admission_whitelist_patterns {
+    name_pattern = "k8s.gcr.io/**"
+  }
+  admission_whitelist_patterns {
+    name_pattern = "gke.gcr.io/**"
+  }
+  admission_whitelist_patterns {
+    name_pattern = "gcr.io/stackdriver-agents/*"
+  }
+  admission_whitelist_patterns {
+    name_pattern = "quay.io/argoproj/*"
+  }
+  admission_whitelist_patterns {
+    name_pattern = "ghcr.io/dexidp/*"
+  }
+  admission_whitelist_patterns {
+    name_pattern = "docker.io/redis[@:]*"
+  }
+  admission_whitelist_patterns {
+    name_pattern = "ghcr.io/external-secrets/*"
+  }
   global_policy_evaluation_mode = "ENABLE"
   default_admission_rule {
     evaluation_mode  = "REQUIRE_ATTESTATION"
